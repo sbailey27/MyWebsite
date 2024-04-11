baa68a6604c410e02355f53ebd5cc6171e387423
sbailey-27 <162745220+sbailey-27@users.noreply.github.com>
2024-04-11 18:18:05 -0400

    Working on javascript for play on mouseover for videos in game page

--- script.js
+++ script.js
@@ -21,3 +21,18 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
+
+// Script for game card videos on games.html page. On mouseover, video starts playing, then resets when the mouse leaves the element
+document.addEventListener("mouseover", function()
+{
+    var myVideo = document.getElementsByClassName("game_card_1_video");
+    playMyVideo();
+
+    function playMyVideo(){
+        for (var i = 0; i < myVideo.length; i++)
+        {
+            myVideo[i].play();
+        }
+        
+    }
+});
